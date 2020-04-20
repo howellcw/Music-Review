5ee80acd4d0ca8a2580038a82c3d9ccda8baa1b1
Caleb Howell <howellcw@vcu.edu>
2020-04-20 19:44:44 -0400

    connection file

---
+++ 508Project/connection.php
@@ -0,0 +1,19 @@
+<?php 
+
+error_reporting(E_ALL);
+ini_set(display_errors, 1);
+
+$servername = "localhost";
+$username = "project_20";
+$password = "V00785650";
+$database = "project_20";
+
+try{
+    $conn = PDO ("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+?>
