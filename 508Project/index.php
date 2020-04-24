3b706a754e2e1f082c49850ed8a49c838479a094
Caleb Howell <howellcw@vcu.edu>
2020-04-24 13:04:03 -0400

    editing index

--- 508Project/index.php
+++ 508Project/index.php
@@ -1,4 +1,10 @@
-<?php 
-// test
-echo "hello";
-?>
+<html>
+</head>
+<title>Music Review</title>
+<body>
+<ul>
+<li><a href = "artistReviews.php">See reviews by artist</a></li>
+<li><a href = "createReview.php">Create a review</a></li>
+</ul>
+</body>
+</html>
