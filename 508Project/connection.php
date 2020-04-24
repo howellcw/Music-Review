acc3cfcdb73d1f85270e0eeb15c0bfb851f693cb
Caleb Howell <howellcw@vcu.edu>
2020-04-24 18:00:09 -0400

    fixed errors in connection and artistReviews

--- 508Project/connection.php
+++ 508Project/connection.php
@@ -9,7 +9,7 @@ $password = "V00785650";
 $database = "project_20";
 
 try{
-    $conn = PDO ("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e){
     echo "Connection failed: " . $e->getMessage();
