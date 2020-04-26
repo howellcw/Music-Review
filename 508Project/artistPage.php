99886c81e71daf219c4f3fc22d92121b98179de8
Caleb Howell <howellcw@vcu.edu>
2020-04-26 19:48:26 -0400

    working query in artistPage

--- 508Project/artistPage.php
+++ 508Project/artistPage.php
@@ -3,14 +3,16 @@ require_once 'connection.php';
 
 $art = $_GET['q'];
 echo "$art";
-$stmt = $conn->query("SELECT title from songs JOIN artists WHERE name = \"$art\"");
+$stmt = $conn->query("SELECT s.title, r.rating, r.review_txt from songs s JOIN reviews r JOIN artists a WHERE a.name = \"$art\"");
 
 echo "<table style='border: solid 1px black;'>";
-echo "<thead><tr><th>Artist</th></tr></thead>";
+echo "<thead><tr><th>Reviews</th></tr></thead>";
 echo "<tbody>";
 
 while ($row = $stmt->fetch_assoc()) {
-    echo $row[title];
+    echo $row['s.title'];
+    echo $row['r.rating'];
+    echo $row['r.review_txt'];
 }
 
 
