430c5cbbb13ff4739769b5c767d8b8027e4d1cc6
Caleb Howell <howellcw@vcu.edu>
2020-04-26 19:40:25 -0400

    simplifying

--- 508Project/artistPage.php
+++ 508Project/artistPage.php
@@ -3,17 +3,14 @@ require_once 'connection.php';
 
 $art = $_GET['q'];
 echo "$art";
-$stmt = $conn->query("SELECT s.title, r.rating, r.review_txt FROM songs s JOIN artists a JOIN reviews r WHERE a.name = '%".$art."%'");
-echo $stmt;
+$stmt = $conn->query("SELECT title from songs WHERE name = '%".$art."%'");
+
 echo "<table style='border: solid 1px black;'>";
 echo "<thead><tr><th>Artist</th></tr></thead>";
 echo "<tbody>";
 
 while ($row = $stmt->fetch_assoc()) {
-    echo $row['s.title'];
-    echo $row['r.rating'];
-    echo $row['r.review_txt'];
-    echo "car";
+    echo $row[title];
 }
 
 
