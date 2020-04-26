c9817159181d05ededb8fa88ce9d73c493c485b2
Caleb Howell <howellcw@vcu.edu>
2020-04-26 19:27:17 -0400

    artist

--- 508Project/artistPage.php
+++ 508Project/artistPage.php
@@ -2,6 +2,7 @@
 require_once 'connection.php';
 
 $art = $_GET['q'];
+echo "$art";
 $stmt = $conn->query("SELECT s.title, r.rating, r.review_txt FROM songs s JOIN artists a JOIN reviews r WHERE a.name = '%".$art."%'");
 
 echo "<table style='border: solid 1px black;'>";
