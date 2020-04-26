44a82a0cf1e7bc7f8475ca9294846692792918bc
Caleb Howell <howellcw@vcu.edu>
2020-04-26 19:45:02 -0400

    fixing query in artistPage

--- 508Project/artistPage.php
+++ 508Project/artistPage.php
@@ -2,8 +2,8 @@
 require_once 'connection.php';
 
 $art = $_GET['q'];
-echo "'%".$art."%'";
-$stmt = $conn->query("SELECT title from songs WHERE name = '%".$art."%'");
+echo "$art";
+$stmt = $conn->query("SELECT title from songs JOIN artists WHERE name = \"$art\"");
 
 echo "<table style='border: solid 1px black;'>";
 echo "<thead><tr><th>Artist</th></tr></thead>";
