c1843646ea164418655ccc02947b60f262902056
Caleb Howell <howellcw@vcu.edu>
2020-04-27 16:35:46 -0400

    undoing changes

--- 508Project/createReviewNext.php
+++ 508Project/createReviewNext.php
@@ -4,24 +4,18 @@ require_once 'connection.php';
 $artReview = $_GET['r'];
 
 $stmt = $conn->query("SELECT al.albumTitle, al.albumID from albums al JOIN artists a ON (al.artistID = a.artistID) WHERE a.name = \"$artReview\"");
-?>
-<html>
-<form>
-<?php
-echo "Album: <select id = 'albumsSelect'  onchange='this.form.submit();'>";
+
+echo "Album: <select id = 'albumsSelect'>";
 
 while ($row = $stmt->fetch_assoc()) {
     echo "<option value = $row[albumID]> $row[albumTitle] </option>";
 }
-?>
-</form>
-</html>
-<?php
+
 $albumSelected = $_GET['albumsSelect'];
 echo "</select>";
 
-$songstmt = $conn->query("SELECT s.title, s.songID from songs s JOIN albums al; ON (s.albumID = al.albumID) WHERE al.albumID = $albumSelected");
-echo "\"$albumSelected\"";
+$songstmt = $conn->query("SELECT s.title, s.songID from songs s JOIN albums al; ON (s.albumID = al.albumID) WHERE al.albumID = \"$albumSelected\"");
+
 echo "Song: <select id = 'songSelected'>";
 
 while ($songrow = $songstmt->fetch_assoc()) {
