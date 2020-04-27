bc5a54f5adec5898311b0ca6caea76b761cfb32a
Caleb Howell <howellcw@vcu.edu>
2020-04-26 20:07:50 -0400

    test

--- 508Project/artistPage.php
+++ 508Project/artistPage.php
@@ -2,15 +2,13 @@
 require_once 'connection.php';
 
 $art = $_GET['q'];
-echo "$art";
-$stmt = $conn->query("SELECT s.title, r.rating, r.review_txt from songs s JOIN reviews r JOIN artists a WHERE a.name = \"$art\"");
 
 echo "<table style='border: solid 1px black;'>";
 echo "<thead><tr><th>Reviews</th></tr></thead>";
 echo "<tbody>";
-
+$stmt = $conn->query("SELECT s.title, r.rating, r.review_txt from songs s JOIN reviews r JOIN artists a WHERE a.name = \"$art\"");
 while ($row = $stmt->fetch_assoc()) {
-    echo $row['title'];
+    echo "<tr><td>$row[title]<tr><td>";
     echo $row['rating'];
     echo $row['review_txt'];
 }
