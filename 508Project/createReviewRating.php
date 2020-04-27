32fb11fbaf2458aa0945039921a9b9b7d6822568
Caleb Howell <howellcw@vcu.edu>
2020-04-27 17:54:02 -0400

    adding createReviewRating

--- 508Project/createReviewRating.php
+++ 508Project/createReviewRating.php
@@ -4,5 +4,11 @@ require_once 'connection.php';
 $selectedSong= $_GET['songSelect'];
 echo $selectedSong;
 
-
+echo "<form action = 'createReviewRating.php'>";
+echo "<label for='quantity'>Quantity (between 1 and 5):</label>";
+echo "<input type='number' id='rating' name='rating' min='1' max='10'>";
+echo "<textarea name='reviewtext' rows='10' cols='30'>Enter your review here.</textarea>";
+echo "<br><br>";
+echo "<input type='submit'>";
+echo "</form>";
 ?>
