fc13ec025d95dc59df3313334f8d4c1482e9c16e
Caleb Howell <howellcw@vcu.edu>
2020-04-27 17:05:42 -0400

    working on review submission page

--- 508Project/createReviewSong.php
+++ 508Project/createReviewSong.php
@@ -1,12 +1,12 @@
 <?php 
 require_once 'connection.php';
 
-$artReview = $_GET['r'];
-
+$selectedAlbum= $_GET['albumSelect'];
+echo "$selectedAlbum";
 $stmt = $conn->query("SELECT al.albumTitle, al.albumID from albums al JOIN artists a ON (al.artistID = a.artistID) WHERE a.name = \"$artReview\"");
 
 echo "<form action = 'createReviewSong.php'>";
-echo "Album: <select name = 'albumsSelect'>";
+echo "Album: <select id = 'albumsSelect'>";
 
 while ($row = $stmt->fetch_assoc()) {
     echo "<option value = $row[albumID]> $row[albumTitle] </option>";
