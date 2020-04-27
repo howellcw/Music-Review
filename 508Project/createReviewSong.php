1ed99c8dda16a126839ff35a0d1a481d5c5eb76f
Caleb Howell <howellcw@vcu.edu>
2020-04-27 17:18:15 -0400

    fixed minor errors in song selection

--- 508Project/createReviewSong.php
+++ 508Project/createReviewSong.php
@@ -2,10 +2,10 @@
 require_once 'connection.php';
 
 $selectedAlbum= $_GET['albumSelect'];
-$stmt = $conn->query("SELECT songID, title FROM songs s JOIN albums a on s.albumID = \"$selectedAlbum\"");
+$stmt = $conn->query("SELECT s.songID, title FROM songs s JOIN albums a on s.albumID = \"$selectedAlbum\"");
 
 echo "<form action = 'submission.php'>";
-echo "Album: <select id = 'albumsSelect'>";
+echo "Song: <select id = 'albumsSelect'>";
 
 while ($row = $stmt->fetch_assoc()) {
     echo "<option value = $row[songID]> $row[title] </option>";
