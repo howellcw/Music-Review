d69142d02d1ca6ff1afdbef0e333c1958b1a9a74
Caleb Howell <howellcw@vcu.edu>
2020-04-26 20:26:57 -0400

    added user name to review

--- 508Project/artistPage.php
+++ 508Project/artistPage.php
@@ -3,10 +3,10 @@ require_once 'connection.php';
 
 $art = $_GET['q'];
 
-$stmt = $conn->query("SELECT s.title, r.rating, r.review_txt from songs s JOIN reviews r JOIN artists a WHERE a.name = \"$art\"");
+$stmt = $conn->query("SELECT s.title, r.rating, r.review_txt, u.name from songs s JOIN reviews r JOIN artists a JOIN users u ON (r.userID = u.userID) WHERE a.name = \"$art\"");
 while ($row = $stmt->fetch_assoc()) {
     echo "<table style='border: solid 1px black;'>";
-    echo "<thead><tr><th>Review</th></tr></thead>";
+    echo "<thead><tr><th>$row[name]</th></tr></thead>";
     echo "<tbody>";
     echo "<tr><td>$row[title]<tr><td>";
     echo "<tr><td>$row[rating]<tr><td>";
