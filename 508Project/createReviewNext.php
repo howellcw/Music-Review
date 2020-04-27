5cec71b739bd800a007db461a6ba303aa771d9c2
Caleb Howell <howellcw@vcu.edu>
2020-04-27 17:25:40 -0400

    hopefully fixed what i broke on createReviewNext

--- 508Project/createReviewNext.php
+++ 508Project/createReviewNext.php
@@ -2,11 +2,11 @@
 require_once 'connection.php';
 
 $artReview = $_GET['r'];
-echo $artReview;
-$stmt = $conn->query("SELECT s.songID, al.albumID,  from albums al JOIN artists a ON (al.artistID = a.artistID) WHERE a.name = \"$artReview\"");
+
+$stmt = $conn->query("SELECT al.albumTitle, al.albumID from albums al JOIN artists a ON (al.artistID = a.artistID) WHERE a.name = \"$artReview\"");
 
 echo "<form action = 'createReviewSong.php'>";
-echo "Album: <select name = 'albumSelect'>";
+echo "Album: <select name = 'albumsSelect'>";
 
 while ($row = $stmt->fetch_assoc()) {
     echo "<option value = $row[albumID]> $row[albumTitle] </option>";
