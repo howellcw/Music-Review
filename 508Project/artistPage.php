d192c2c69563574872dc1a37be58e1c772a56992
Caleb Howell <howellcw@vcu.edu>
2020-04-26 20:10:31 -0400

    fixing table

--- 508Project/artistPage.php
+++ 508Project/artistPage.php
@@ -9,8 +9,8 @@ echo "<tbody>";
 $stmt = $conn->query("SELECT s.title, r.rating, r.review_txt from songs s JOIN reviews r JOIN artists a WHERE a.name = \"$art\"");
 while ($row = $stmt->fetch_assoc()) {
     echo "<tr><td>$row[title]<tr><td>";
-    echo $row['rating'];
-    echo $row['review_txt'];
+    echo "<tr><td>$row[rating]<tr><td>";
+    echo "<tr><td>$row[review_txt]<tr><td>";
 }
 
 
