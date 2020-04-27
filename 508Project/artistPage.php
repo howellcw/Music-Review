deb554284dc5892e20bf34d43482b0694acc452b
Caleb Howell <howellcw@vcu.edu>
2020-04-26 20:12:35 -0400

    changed format of table for artistPage

--- 508Project/artistPage.php
+++ 508Project/artistPage.php
@@ -3,20 +3,17 @@ require_once 'connection.php';
 
 $art = $_GET['q'];
 
-echo "<table style='border: solid 1px black;'>";
-echo "<thead><tr><th>Reviews</th></tr></thead>";
-echo "<tbody>";
 $stmt = $conn->query("SELECT s.title, r.rating, r.review_txt from songs s JOIN reviews r JOIN artists a WHERE a.name = \"$art\"");
 while ($row = $stmt->fetch_assoc()) {
+    echo "<table style='border: solid 1px black;'>";
+    echo "<thead><tr><th>Review</th></tr></thead>";
+    echo "<tbody>";
     echo "<tr><td>$row[title]<tr><td>";
     echo "<tr><td>$row[rating]<tr><td>";
     echo "<tr><td>$row[review_txt]<tr><td>";
+    echo "</tbody>";
+    echo "</table>";
 }
-
-
-
-echo "</tbody>";
-echo "</table>";
 
 
 ?>
