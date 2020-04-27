8e519292ffbf3c6f9425b9a572896d85db10ca9f
Caleb Howell <howellcw@vcu.edu>
2020-04-26 20:32:21 -0400

    undid formatting changes

--- 508Project/artistPage.php
+++ 508Project/artistPage.php
@@ -3,13 +3,12 @@ require_once 'connection.php';
 
 $art = $_GET['q'];
 
-$stmt = $conn->query("SELECT s.title, r.rating, r.review_txt, u.name from songs s JOIN reviews r JOIN artists a JOIN users u ON (r.userID = u.userID) JOIN albums al ON (song.albumID = al.albumID) WHERE a.name = \"$art\"");
+$stmt = $conn->query("SELECT s.title, r.rating, r.review_txt, u.name from songs s JOIN reviews r JOIN artists a JOIN users u ON (r.userID = u.userID) WHERE a.name = \"$art\"");
 while ($row = $stmt->fetch_assoc()) {
     echo "<table style='border: solid 1px black;'>";
     echo "<thead><tr><th>$row[name]</th></tr></thead>";
     echo "<tbody>";
     echo "<tr><td>Song: $row[title]<tr><td>";
-    echo "<tr><td>Album: $row[title]<tr><td>";
     echo "<tr><td>Rating: $row[rating]<tr><td>";
     echo "<tr><td>Review: $row[review_txt]<tr><td>";
     echo "</tbody>";
