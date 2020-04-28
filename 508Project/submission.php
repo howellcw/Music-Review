762559ea76efa6782287a3af258d6a7e4bace84d
Caleb Howell <howellcw@vcu.edu>
2020-04-28 15:17:07 -0400

    should have creating a review completed and working

--- 508Project/submission.php
+++ 508Project/submission.php
@@ -1,14 +1,14 @@
 <?php 
 session_start();
 require_once 'connection.php';
+
 $selectedSong= $_GET['songSelect'];
 $ratingNum = $_GET['rating'];
 $rvw = $_GET['reviewtext'];
 $uid = $_SESSION['user_id'];
 
+$sql = "INSERT INTO reviews (songID, userID, rating, review_txt) VALUES ('$selectedSong', '$uid', '$ratingNum', '$rvw');";
+mysqli_query($conn, $sql);
 
-echo $selectedSong;
-echo $ratingNum;
-echo $rvw;
-echo $uid;
+echo "<a href='index.php'>Success!!!</a>";
 ?>
