7f89e8cfbbf3e92a978fdf28e7fec580371366a4
Caleb Howell <howellcw@vcu.edu>
2020-04-28 14:52:11 -0400

    cut out createReviewRating

--- 508Project/createReviewSong.php
+++ 508Project/createReviewSong.php
@@ -4,7 +4,7 @@ require_once 'connection.php';
 $selectedAlbum= $_GET['albumSelect'];
 $stmt = $conn->query("SELECT DISTINCT s.songID, s.title FROM songs s JOIN albums a on s.albumID = $selectedAlbum");
 
-echo "<form method = 'get' action = 'createReviewRating.php'>";
+echo "<form method = 'get' action = 'submission.php'>";
 echo "Song: <select name = 'songSelect'>";
 
 while ($row = $stmt->fetch_assoc()) {
@@ -12,7 +12,13 @@ while ($row = $stmt->fetch_assoc()) {
 }
 echo " <input type='submit' value='Submit'/>";
 echo "</select>";
-
+echo "<label for='quantity'>Your rating (between 1 and 10):</label>";
+echo "<input type='number' id='rating' name='rating' min='1' max='10'>";
+echo "<br><br>";
+echo "<textarea name='reviewtext' rows='10' cols='30'>Enter your review here.</textarea>";
+echo "<br><br>";
+echo "<input type='submit'>";
+echo "</form>";
 echo "</form>";
 
 
