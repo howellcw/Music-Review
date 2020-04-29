b7ef2bcec06f3154092573ca797d1c1c3356d2f4
Caleb Howell <howellcw@vcu.edu>
2020-04-29 14:18:59 -0400

    artist statistics

--- 508Project/artistStatistics.php
+++ 508Project/artistStatistics.php
@@ -3,12 +3,18 @@
 require_once 'connection.php';
 
 $selArt= $_GET['choose'];
-echo $selArt;
+
+$sql = $conn->query("Select artistID from artists where name = "$selArt";");
+while ($row = $stmt->fetch_assoc()) {
+    $selArtID = $row[artistID];
+}
+
+echo $selArtID;
 
 $stmt = $conn->query("SELECT CAST(AVG(rating) AS DECIMAL (2,1)) AS averageRating FROM reviews r JOIN songs s ON r.songID = s.songID JOIN artists a ON (s.artistID = 1)");
 
 while ($row = $stmt->fetch_assoc()) {
-    echo "Average Rating for Artist: $row[averageRating]";
+    echo "Average Rating for $selArt: $row[averageRating]";
 }
 
 
