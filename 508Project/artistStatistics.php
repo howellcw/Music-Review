55ecbe3aac3315b0c721aaba3c51c717e5383e7d
Caleb Howell <howellcw@vcu.edu>
2020-04-29 14:00:39 -0400

    fixed error with query

--- 508Project/artistStatistics.php
+++ 508Project/artistStatistics.php
@@ -5,7 +5,7 @@ require_once 'connection.php';
 $selArt= $_GET['choose'];
 
 $stmt = $conn->query(" SELECT CAST(AVG(rating) AS DECIMAL (2,1)) AS averageRating FROM reviews r JOIN songs s ON r.songID = s.songID JOIN artists a ON (s.artistID = 1)\"$selArt\"");
-mysqli_query($conn, $sql);
+mysqli_query($conn, $stmt);
 
 $row = $stmt->fetch_assoc();
 echo "Average Rating for Artist: $row[averageRating]";
