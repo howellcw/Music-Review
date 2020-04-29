66e527c4122f48f8ce7e0f4f436d82cfb42d1ad0
Caleb Howell <howellcw@vcu.edu>
2020-04-29 14:25:01 -0400

    hopefully functional display of average artist rating

--- 508Project/artistStatistics.php
+++ 508Project/artistStatistics.php
@@ -9,9 +9,8 @@ while ($row = $sql->fetch_assoc()) {
     $selArtID = $row['artistID'];
 }
 
-echo $selArtID;
 
-$stmt = $conn->query("SELECT CAST(AVG(rating) AS DECIMAL (2,1)) AS averageRating FROM reviews r JOIN songs s ON r.songID = s.songID JOIN artists a ON (s.artistID = 1)");
+$stmt = $conn->query("SELECT CAST(AVG(rating) AS DECIMAL (2,1)) AS averageRating FROM reviews r JOIN songs s ON r.songID = s.songID JOIN artists a ON (s.artistID = \"$selArtID\")");
 
 while ($row = $stmt->fetch_assoc()) {
     echo "Average Rating for $selArt: $row[averageRating]";
