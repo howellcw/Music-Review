e79f278c92a9850a0b987902d81b700fda661be1
Caleb Howell <howellcw@vcu.edu>
2020-04-29 14:05:08 -0400

    hardcoding something to test

--- 508Project/artistStatistics.php
+++ 508Project/artistStatistics.php
@@ -4,7 +4,7 @@ require_once 'connection.php';
 
 $selArt= $_GET['choose'];
 
-$stmt = $conn->query("SELECT CAST(AVG(rating) AS DECIMAL (2,1)) AS averageRating FROM reviews r JOIN songs s ON r.songID = s.songID JOIN artists a ON (s.artistID = \"$selArt\")");
+$stmt = $conn->query("SELECT CAST(AVG(rating) AS DECIMAL (2,1)) AS averageRating FROM reviews r JOIN songs s ON r.songID = s.songID JOIN artists a ON (s.artistID = 1)");
 mysqli_query($conn, $stmt);
 
 while ($row = $stmt->fetch_assoc()) {
