d5c4cab8e6b6b780a2b5800fbbcc192be31e5273
Caleb Howell <howellcw@vcu.edu>
2020-04-29 14:13:17 -0400

    weird warning

--- 508Project/artistStatistics.php
+++ 508Project/artistStatistics.php
@@ -6,12 +6,16 @@ $selArt= $_GET['choose'];
 echo $selArt;
 
 $stmt = $conn->query("SELECT CAST(AVG(rating) AS DECIMAL (2,1)) AS averageRating FROM reviews r JOIN songs s ON r.songID = s.songID JOIN artists a ON (s.artistID = 1)");
-mysqli_query($conn, $stmt);
+$response = mysqli_query($conn, $stmt);
 
-while ($row = $stmt->fetch_assoc()) {
-    echo "Average Rating for Artist: $row[averageRating]";
+if (empty($response)) {
+    die('Error: ' mysqli_error($conn));
 }
-
+else {
+    while ($row = $stmt->fetch_assoc()) {
+        echo "Average Rating for Artist: $row[averageRating]";
+    }
+}
 
 
 
