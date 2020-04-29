22da04fa2e1707d37d1a5cbf07844eb1d11ae881
Caleb Howell <howellcw@vcu.edu>
2020-04-29 14:08:04 -0400

    testing echo

--- 508Project/artistStatistics.php
+++ 508Project/artistStatistics.php
@@ -3,6 +3,7 @@
 require_once 'connection.php';
 
 $selArt= $_GET['choose'];
+echo $selArt;
 
 $stmt = $conn->query("SELECT CAST(AVG(rating) AS DECIMAL (2,1)) AS averageRating FROM reviews r JOIN songs s ON r.songID = s.songID JOIN artists a ON (s.artistID = 1)");
 mysqli_query($conn, $stmt);
