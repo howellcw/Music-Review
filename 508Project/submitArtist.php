2e6b5136be809cc31bafc0e38918c8b2f7b14b41
Caleb Howell <howellcw@vcu.edu>
2020-05-01 21:39:54 -0400

    forgot to execute query

--- 508Project/submitArtist.php
+++ 508Project/submitArtist.php
@@ -4,8 +4,8 @@ require_once 'connection.php';
 
 $addArtist = mysqli_real_escape_string($conn, $_POST['artist']);
 
-$sql = "INSERT INTO artists (name) VALUES ($addArtist);";
-
+$sql = "INSERT INTO artists (name) VALUES ('$addArtist');";
+mysqli_query($conn, $sql);
 
 echo "<a href='index.php'>Successfully added $addArtist!</a>";
 ?>
