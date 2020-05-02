523aa305ac908bf78f3dfd5074d11cdaf49a7bfa
Caleb Howell <howellcw@vcu.edu>
2020-05-01 21:09:35 -0400

    tables on artist statistics page

--- 508Project/artistStatistics.php
+++ 508Project/artistStatistics.php
@@ -13,16 +13,20 @@ while ($row = $sql->fetch_assoc()) {
 $stmt = $conn->query("SELECT CAST(AVG(rating) AS DECIMAL (2,1)) AS averageRating FROM reviews r JOIN songs s ON r.songID = s.songID JOIN artists a ON (s.artistID = \"$selArtID\")");
 
 while ($row = $stmt->fetch_assoc()) {
+    echo "<table style='border: solid 1px black;'>";
     echo "Average Rating for $selArt: $row[averageRating]";
+    echo "</table>";
 }
 
 $avgstmt = $conn->query("SELECT s.title, CAST(AVG(r.rating) AS DECIMAL (2,1)) AS avg_rating FROM songs s JOIN reviews r ON (s.songID = r.songID) JOIN artists a WHERE (s.artistID = \"$selArtID\") GROUP BY s.title ORDER BY AVG(r.rating) DESC LIMIT 10");
 
 echo "Top 10 Rated Songs:\n";
 $count = 1;
+echo "<table style='border: solid 1px black;'>";
 while ($row = $avgstmt->fetch_assoc()) {
     echo "$count) $row[title] $row[avg_rating]\n";
     $count += 1;
 }
+echo "</table";
 
 ?>
