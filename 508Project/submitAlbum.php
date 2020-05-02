2ada4b13944ca160eb2587b3b7652101854267a9
Caleb Howell <howellcw@vcu.edu>
2020-05-01 22:05:53 -0400

    more errors

--- 508Project/submitAlbum.php
+++ 508Project/submitAlbum.php
@@ -5,13 +5,10 @@ require_once 'connection.php';
 $alArt = mysqli_real_escape_string($conn, $_POST['albumArtist']);
 $addAl = mysqli_real_escape_string($conn, $_POST['album']);
 
-$stmt = "Select artistID from artists where name = '$alArt';";
-mysqli_query($conn, $stmt);
-
+$stmt = $conn->query("Select artistID from artists where name = '$alArt'");
 while ($row = $stmt->fetch_assoc()) {
     $selAlArt = $row['artistID'];
 }
-
 
 $sql = "INSERT INTO albums (artistID, albumTitle) VALUES ('$selAlArt', $addAl);";
 mysqli_query($conn, $sql);
