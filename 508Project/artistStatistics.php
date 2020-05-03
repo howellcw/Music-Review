d521b2f1a7962752d159e4be1abaef8fa39ca6df
Caleb Howell <howellcw@vcu.edu>
2020-05-03 15:34:36 -0400

    added more to artist statistics

--- 508Project/artistStatistics.php
+++ 508Project/artistStatistics.php
@@ -35,4 +35,15 @@ while ($row = $avgstmt->fetch_assoc()) {
 echo "</tbody>";
 echo "</table";
 
+$albmstmt = $conn->query("SELECT al.albumTitle, AVG(r.rating) as al_rating from reviews r join songs s on r.songID = s.songID join albums al on s.albumID = al.albumID join artists a on al.artistID = a.artistID where a.artistID = \"$selArtID\" group by al.albumID");
+
+echo "<table style='border: solid 1px black;'>";
+echo "<thead><tr><th>Average Rating by Album</th></tr></thead>";
+echo "<tbody>";
+while ($row = $albmstmt->fetch_assoc()) {
+    echo "<tr><td>$row[albumTitle]: $row[al_rating]<tr><td>";
+}
+echo "</tbody>";
+echo "</table";
+
 ?>
